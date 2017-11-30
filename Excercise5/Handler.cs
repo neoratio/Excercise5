f22e7db918342cd65ee57caf3679d3695f53d2bf
neoratio <stefan_seyer@hotmail.com>
2017-11-30 18:26:03 +0100

    test

---
+++ Excercise5/Handler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class Handler
+    {
+        TcpClient client = new TcpClient();
+        Socket Socket;
+
+        public Handler()
+        {
+            client.Connect(new IPEndPoint(IPAddress.Loopback, 10100));
+            Socket = client.Client;
+        }
+
+        public void SendMessage(string message)
+        {
+            Socket.Send(Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
--- Excercise5/Program.cs
+++ Excercise5/Program.cs
@@ -10,6 +10,7 @@ namespace Excercise5
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
